3caf0913c7cdbe654bf74a8c1b87a757f4fcae64
Aleksander Schei <Aleksanderschei@gmail.com>
2022-03-15 12:36:49 +0100

    La til en github linje i arvinger

---
+++ github/arving.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 18:29:50 2022
+
+@author: 47944
+"""
+
+#klassediagrammer
+#UML
+#-Navn på klasse
+#-Hva du får ut av klassen
+#-funksjoner i klassen
+
+#egenskaper objektorientertrte programmer
+#Hver klasse har et ansvarsområde
+#hver klasse er uavhengig
+#En endring bør påvrike så få klasser som mulig
+
+#arving
+#uviktig
+
+#instanceof for å sjekke om noe er TRUE eller False
+# eks = isinstance(5,int)
+# kan sjeke om en variabel hører hjemme i en klasse eller ei
+#
+
+#overstyring av metoder
+#I arving kan du override metoder senere i arvingen i den nye koden, hvis klassen du arver har en funksjon du vil endre
+# 
+#liskovs substitusjon prinsipp
+#
